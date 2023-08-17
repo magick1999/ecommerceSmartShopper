8a0ee3afba098286d83cce8a47546e2bdabefacf
ionica cercel <tianbogdan@gmail.com>
2023-08-17 19:04:31 +0300

    resetted packages

--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
+const withSass = require("@zeit/next-sass");
+const withLess = require("@zeit/next-less");
+
 const isProd = process.env.NODE_ENV === "production";
 
+// fix: prevents error when .less files are required by node
+if (typeof require !== "undefined") {
+  require.extensions[".less"] = (file) => {};
+}
 
-module.exports = {
-  env: {
-    PUBLIC_URL: "",
-  },
-  cssModules: true, // Enable CSS modules if needed
-  lessLoaderOptions: {
-    javascriptEnabled: true,
-  },
-};
+module.exports = withLess(
+  withSass({
+    env: {
+      PUBLIC_URL: "",
+    },
+    lessLoaderOptions: {
+      javascriptEnabled: true,
+    },
+  })
+);
