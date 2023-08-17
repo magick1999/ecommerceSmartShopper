993831ae366d52da0adbf74e9d5f739e05f1b498
ionica cercel <tianbogdan@gmail.com>
2023-08-17 16:37:25 +0300

    removed stupid dependencies

--- next.config.js
+++ next.config.js
@@ -1,20 +1,12 @@
-const withSass = require("@zeit/next-sass");
-const withLess = require("@zeit/next-less");
-
 const isProd = process.env.NODE_ENV === "production";
 
-// fix: prevents error when .less files are required by node
-if (typeof require !== "undefined") {
-  require.extensions[".less"] = (file) => {};
-}
 
-module.exports = withLess(
-  withSass({
-    env: {
-      PUBLIC_URL: "",
-    },
-    lessLoaderOptions: {
-      javascriptEnabled: true,
-    },
-  })
-);
+module.exports = {
+  env: {
+    PUBLIC_URL: "",
+  },
+  cssModules: true, // Enable CSS modules if needed
+  lessLoaderOptions: {
+    javascriptEnabled: true,
+  },
+};
